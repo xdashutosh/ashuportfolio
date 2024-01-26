e5b05fcdd9074202798b3f8a1d522a657615b193
ashutosh chaudhary <ashuchaudhary6969@gmail.com>
2024-01-26 20:02:24 +0530

    social links

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
+import { useState,useEffect } from 'react';
 import './App.css';
+import {HStack,Heading,Text,VStack} from '@chakra-ui/react';
+import { BrowserRouter ,Routes,Route } from 'react-router-dom';
+import Header from './Header';
+import Home from './Home';
+import Skills from './Skills';
+import Projects from './Projects';
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  const [showName, setShowName] = useState(true);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setShowName(false);
+    }, 4000);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
+
+
+
+  return showName?(
+    
+    <HStack w={'full'} h={'100vh'} bgColor={'#28282b'}justifyContent={'center'} alignItems={'center'}>
+
+      <div class="wrapper">
+  <h1 class="glitch">
+
+  <Text  fontSize={'7xl'} fontWeight={'bolder'} textColor={'white'} >Ɔ<b style={{color:'#90ee90'}}>∀</b></Text>
+
+  </h1>
+</div>
+
+
+    </HStack>
+  ): (
+  <BrowserRouter>
+  <Header/>
+  <Routes>
+  <Route path='/skills' element={<Skills/>}/>
+  <Route path="/" element={<Home/>}/>
+  <Route path ="/projects" element={<Projects/>}/>
+  </Routes>
+  </BrowserRouter>
+
   );
 }
 
